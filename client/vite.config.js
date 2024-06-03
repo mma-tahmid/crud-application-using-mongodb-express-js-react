e3af6754b21325a2342c5e2f39ea748eca6a6e90
mma-tahmid <mma.tahmid@gmail.com>
2024-06-03 18:51:58 +0600

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+//  https://vitejs.dev/config/,
 export default defineConfig({
   plugins: [react()],
   // build: {
-  //   outDir: 'dist',
+  //   outDir: 'dist'
   // },
 
   server: {
     proxy: {
       // je api endpoint dia start hobe seta bujia diar jonno
       '/api/': {
-        // target: 'http://localhost:8000',
-        target: 'https://crud-application-using-mongodb-express.onrender.com',
+        target: 'http://localhost:8000',
+
+        //target: 'https://crud-application-using-mongodb-express.onrender.com',
 
         // To avoid SSL/TLS certificate verification process. 
         changeOrigin: true,
         secure: false, // This option allows to ignore SSL issues
-
+        // rewrite: (path) => path.replace(/^\/api/, '/api'),
+        // secure: true,
         // target: `http://localhost:${process.env.VITE_PORT}` no need
       }
     }
