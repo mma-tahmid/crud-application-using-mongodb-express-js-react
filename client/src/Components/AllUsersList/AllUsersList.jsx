0c9bcc68b6f13e0fd2b5d5c85867ab56be9e12de
mma-tahmid <mma.tahmid@gmail.com>
2024-06-03 14:28:59 +0600

    solved deployment issues

--- client/src/Components/AllUsersList/AllUsersList.jsx
+++ client/src/Components/AllUsersList/AllUsersList.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios'
 import toast from 'react-hot-toast';
-import { FaRegEdit } from "react-icons/fa";
 
 const AllUsersList = () => {
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // build: {
+  //   outDir: 'dist',
+  // },
 
   server: {
     proxy: {
       // je api endpoint dia start hobe seta bujia diar jonno
       '/api/': {
-        // target: 'http://localhost:8000'  (for localhost) 
+        // target: 'http://localhost:8000',
         target: 'https://crud-application-using-mongodb-express.onrender.com',
 
         // To avoid SSL/TLS certificate verification process. 
