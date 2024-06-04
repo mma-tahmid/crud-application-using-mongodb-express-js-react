400ae74a093b6720c4bf1b593bf69082eb4e6383
mma-tahmid <mma.tahmid@gmail.com>
2024-06-04 21:16:53 +0600

    solved deoployed issued

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 //  https://vitejs.dev/config/,
 export default defineConfig({
+
   plugins: [react()],
   // build: {
   //   outDir: 'dist'
@@ -12,14 +13,14 @@ export default defineConfig({
     proxy: {
       // je api endpoint dia start hobe seta bujia diar jonno
       '/api/': {
-        target: 'http://localhost:8000',
+        //target: 'http://localhost:8000',
 
-        //target: 'https://crud-application-using-mongodb-express.onrender.com',
+        target: 'https://crud-application-using-mongodb-express.onrender.com',
 
         // To avoid SSL/TLS certificate verification process. 
         changeOrigin: true,
-        secure: false, // This option allows to ignore SSL issues
-        // rewrite: (path) => path.replace(/^\/api/, '/api'),
+        secure: false, // This option allows to ignore SSL issues // default false
+        //rewrite: (path) => path.replace(/^\/api/, '/api'),
         // secure: true,
         // target: `http://localhost:${process.env.VITE_PORT}` no need
       }
